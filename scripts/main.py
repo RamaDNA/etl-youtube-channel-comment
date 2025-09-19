3b266327f863502b3440f6bb69e0c6a5f950533b
nofian bismika ariyatma <rama99ever@gmail.com>
2025-09-19 18:17:55 +0700

    re foldering and add gcp

--- scripts/main.py
+++ scripts/main.py
@@ -1,7 +1,8 @@
 #class function
-from youtube_api import YouTubeAPI
-from dataset_comment_transform import TransformDataset
-from data_transform_pipeline import YouTubeDataPipeline
+from extract.extract_youtube_comments import YouTubeAPI
+from transform.preprocessing_data import TransformDataset
+from transform.transform_pipeline import YouTubeDataPipeline
+from load.load_to_bigquery import BigQueryHelper
 
 
 #library
@@ -10,6 +11,23 @@ import os
 
 # === Masukkan API KEY dan Channel ID di sini ===
 load_dotenv()   # path di dalam container
+
+#SET GOOGLE_APPLICATION_CREDENTIALS BIGQUERY
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = os.getenv("GOOGLE_APPLICATION_CREDENTIALS")
+project_id = os.getenv("PROJECT_ID")
+dataset_id = os.getenv("DATASET_ID")
+#DATA STAGING
+table_id_staging = os.getenv("TABLE_ID_STAGING")
+csv_file_staging = os.getenv("CSV_FILE_STAGING")
+#DATA INTERMEDIATE
+table_id_intermediate = os.getenv("TABLE_ID_INTERMEDIATE")
+csv_file_intermediate = os.getenv("CSV_FILE_INTERMEDIATE")
+#DATA MART
+table_id_mart = os.getenv("TABLE_ID_MART")
+csv_file_mart = os.getenv("CSV_FILE_MART")
+#DATA WORD FREQ
+table_id_word_freq = os.getenv("TABLE_ID_WORD_FREQ")
+csv_file_word_freq = os.getenv("CSV_FILE_WORD_FREQ")
 
 def main():
     yt = YouTubeAPI(os.getenv("YOUTUBE_API_KEY"), os.getenv("CHANNEL_ID"))
@@ -28,10 +46,10 @@ def main():
         all_comments.extend(comments)
 
     # Simpan ke CSV
-    yt.save_to_csv(all_comments, "latest_video_comments.csv")
+    yt.save_to_csv(all_comments, "raw_data_latest_video_comments.csv")
 
     # preprocessing CSV
-    transform = TransformDataset("latest_video_comments.csv")
+    transform = TransformDataset("raw_data_latest_video_comments.csv")
     transform.save_to_csv("processed/staging.csv")
 
     pipeline = YouTubeDataPipeline("processed/staging.csv", output_dir="processed")
@@ -47,6 +65,18 @@ def main():
     # Data Mart
     pipeline.create_data_mart()
     pipeline.save_data_mart()
+
+    # Load data to BigQuery
+    bq = BigQueryHelper(project_id=project_id, location="asia-southeast2")
+
+    #Staging
+    bq.load_csv_to_bigquery(dataset_id, table_id_staging, csv_file_staging)
+    #Intermediate
+    bq.load_csv_to_bigquery(dataset_id, table_id_intermediate, csv_file_intermediate)
+    #Mart
+    bq.load_csv_to_bigquery(dataset_id, table_id_mart, csv_file_mart)
+    #Word freq
+    bq.load_csv_to_bigquery(dataset_id, table_id_word_freq, csv_file_word_freq)
 
 
 
